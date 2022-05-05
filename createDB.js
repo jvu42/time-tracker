19315398e0472da5aef58f63b2ae94ab0ac92121
James <jvu42@wuo.ca>
2022-05-05 16:15:01 -0400

    created a database to store active window data.

---
+++ createDB.js
@@ -0,0 +1,43 @@
+var sqlite3 = require('sqlite3');
+let db = new sqlite3.Database('./data/aw.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err && err.code == "SQLITE_CANTOPEN") {
+        createDatabase();
+        return;
+        } else if (err) {
+            console.log("Getting error " + err);
+            exit(1);
+    }
+    dbReady(db);
+    // runQueries(db);
+});
+
+function createDatabase() {
+    db = new sqlite3.Database('data/aw.db', (err) => {
+        if (err) {
+            console.log("Getting error " + err);
+            exit(1);
+        }
+        createTables(db);
+    });
+}
+
+function createTables(db) {
+    db.exec(`
+    create table windowUsage (
+        appName text not null,
+        timestamp integer not null,
+        duration integer not null
+    );
+    
+        `
+    );
+    dbReady(db);
+}
+
+function dbReady(db){
+    db.all(`SELECT * FROM windowUsage`,(err,rows) => {
+        console.log(rows);
+        console.log(`Printing rows from windowUsage table`)
+    });
+
+}
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const INTERVAL = 1000 *10;
 const activeWindow = require('active-win');
 
 setInterval(async () => {
-    console.log("Hello World");
-    console.log(await activeWindow());
+    // console.log("Hello World");
+    // console.log(await activeWindow());
+    // const trackWindow = (await activeWindow());
+    // debugger
 }, INTERVAL);
+
+const sqlite3 = require('sqlite3');
+
