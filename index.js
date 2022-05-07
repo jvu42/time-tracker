d7560ed51bc6d15b7109fb2b5674727b0618f58b
James <jvu42@wuo.ca>
2022-05-06 23:14:13 -0400

    Can insert and replace existing values to the table

--- index.js
+++ index.js
@@ -11,26 +11,57 @@ if (!table['count(*)']) {
     sql.prepare(`
     create table windowUsage (
         appName text not null,
-        timestamp integer not null,
+        timestamp text not null,
         duration integer not null
     );
     
         `).run();
 }
 let insertStmt = sql.prepare(`INSERT INTO windowUsage (appName, timestamp, duration) VALUES (?, ?, ?)`);
-insertStmt.run('Google Chrome',1010, 2020);
-insertStmt.run('Visual Studio Code', 2020, 3030);
+let updateStmt = sql.prepare(`UPDATE windowUsage SET timestamp = ?, duration = ? WHERE appName = ?`);
+// insertStmt.run('Testing',getTimestamp(), 2020);
+// insertStmt.run('Visual Studio Code', 2020, 3030);
 let dbInfo = sql.prepare(`SELECT * FROM windowUsage`).all();
 console.log(dbInfo);
 
 // console.log("Hello World");
 
-// setInterval(async () => {
-//     console.log(await activeWindow());
-//     const trackWindow = (await activeWindow());
-//     console.log(trackWindow.owner.name);
-//     console.log()
-// }, INTERVAL);
+setInterval(async () => {
+    //console.log(await activeWindow());
+    const trackWindow = (await activeWindow());
+    let currentApp = trackWindow.owner.name;
+    addData(currentApp);
+    console.log(trackWindow.owner.name);
+    dbInfo = sql.prepare(`SELECT * FROM windowUsage`).all();
+    console.log(dbInfo);
+}, INTERVAL);
 
 
 
+function addData(currentApp) {
+    //Get timestamp and current duration here
+    let timestamp = getTimestamp();
+    let duration = getDuration(currentApp);
+    if (duration == INTERVAL){
+        insertStmt.run(currentApp,timestamp,duration);
+    }
+    else{
+        updateStmt.run(timestamp,duration,currentApp);
+    }
+}
+
+function getTimestamp(){
+    let today = new Date();
+    let todayDate = `${today.getMonth() + 1}-${today.getDate()}-${today.getFullYear()}`;
+    let todayTime = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`
+    return `${todayDate} --- ${todayTime}`;
+}
+
+function getDuration(currentApp) {
+    const durationStmt = sql.prepare('SELECT * FROM windowUsage WHERE appName = ?').iterate(currentApp);
+    let counter = INTERVAL
+    for (const record of durationStmt){
+        counter = record.duration + INTERVAL;
+    }
+    return counter;
+}
